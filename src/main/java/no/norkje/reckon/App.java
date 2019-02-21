0131ee218ccff8c1f0e6a89978eb2cc81a945942
Kjell Johan Nordstrand <kjn@driw.no>
2019-02-21 10:39:07 +0100

    Add feature 2

--- src/main/java/no/norkje/reckon/App.java
+++ src/main/java/no/norkje/reckon/App.java
@@ -4,9 +4,14 @@ public class App {
 
     public static void main(String[] args) {
         feature1();
+        feature2();
     }
 
     public static void feature1() {
         System.out.println("Feature 1");
+    }
+
+    public static void feature2() {
+        System.out.println("Feature 2");
     }
 }
