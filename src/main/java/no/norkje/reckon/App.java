ed96d1be8d971b92ddf150973f3bdf9840902b00
Kjell Johan Nordstrand <kjn@driw.no>
2019-02-21 10:38:28 +0100

    Add feature 1

--- src/main/java/no/norkje/reckon/App.java
+++ src/main/java/no/norkje/reckon/App.java
@@ -3,6 +3,10 @@ package no.norkje.reckon;
 public class App {
 
     public static void main(String[] args) {
+        feature1();
+    }
 
+    public static void feature1() {
+        System.out.println("Feature 1");
     }
 }
